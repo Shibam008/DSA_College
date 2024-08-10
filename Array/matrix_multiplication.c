0d5890b1258356bd7bef1e9da941ef6380d80249
Shibam Sadhukhan <sadhukhan799@gmail.com>
2024-08-10 11:41:38 +0530

    matrix

---
+++ Array/matrix_multiplication.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main() {
+    int row1, row2, col1, col2;
+    printf("\tEnter details of first matrix : \n");
+    printf("Enter number of rows and columns : ");
+    scanf("%d %d", &row1, &col1);
+    printf("Enter elements for first matrix -> \n");
+    int mat1[row1][col1];
+    for(int i=0; i<row1; i++) {
+        for(int j=0; j<col1; j++) {
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+
+    printf("\tEnter details of second matrix : \n");
+    printf("Enter number of rows and columns : ");
+    scanf("%d %d", &row2, &col2);
+    printf("Enter elements for second matrix -> \n");
+    int mat2[row2][col2];
+    for(int i=0; i<row2; i++) {
+        for(int j=0; j<col2; j++) {
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+
+    if(col1 != row2) 
+    {
+        printf("Matrix multiplication can't be performed!\nWe need square matrix");
+    }
+    else 
+    {
+        int res[row1][col2];
+        printf("\nMultiplication  of two matrices : \n");
+
+        for(int i=0; i<row1; i++) 
+        {
+            for(int j=0; j<col2; j++) 
+            {
+                res[i][j] = 0;
+                for(int k=0; k<col1; k++) 
+                {
+                    res[i][j] += mat1[i][k] * mat2[k][j];
+                }
+                printf("%d ", res[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
